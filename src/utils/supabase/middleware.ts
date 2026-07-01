0a0b01a00fc5e5d66e2fec5a2f0149a626c9a67d
pruthvi SB <sbpruthvi.07@gmail.com>
2026-07-01 07:10:48 +0530

    Fix middleware initialization parameters to correctly propagate request cookies

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -5,11 +5,8 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
 
 export async function updateSession(request: NextRequest) {
-  // Create an unmodified response
   let supabaseResponse = NextResponse.next({
-    request: {
-      headers: request.headers,
-    },
+    request,
   });
 
   const supabase = createServerClient(
